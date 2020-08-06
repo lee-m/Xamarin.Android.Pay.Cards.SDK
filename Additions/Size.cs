573a39f6199f392c21c4c02b218108a52cfbbfa1
Lee Millward <lee.millward@mystaffshop.com>
2020-08-06 09:43:16 +0100

    Initial commit

---
+++ Additions/Size.cs
@@ -0,0 +1,10 @@
+﻿namespace Cards.Pay.Paycardsrecognizer.Sdk.Utils
+{
+    public partial class Size : global::Java.Lang.Object, global::Java.Lang.IComparable
+    {
+        public int CompareTo(Java.Lang.Object o)
+        {
+            return 0;
+        }
+    }
+}
